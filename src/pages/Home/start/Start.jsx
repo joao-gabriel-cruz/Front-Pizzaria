ae7d6f5fc47e4d72eff37b169cdb5d152d92cd7c
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-06-10 11:24:51 -0300

    Componet Button criado

--- src/pages/Home/start/Start.jsx
+++ src/pages/Home/start/Start.jsx
@@ -1,8 +1,10 @@
 
 import { Link } from  'react-router-dom';
+import Button from '../../../components/button/button';
 import './Start.css';
 
 function Start() {
+  const navigate = useNavigate();
   return (
     <section className="Home">
         <div className='textHome'>
@@ -14,8 +16,8 @@ function Start() {
               nihil error, voluptatem fugit dolor consequatur.
             </p>
             <div className='links'>
-              <Link to=''><button>Pedir Online</button></Link>
-              <Link to='' >Ver Menu</Link>
+              <Link to=''><Button functionClick={() => navigate('') } className='button' name='Pedir online'/></Link>
+              <Link to='' > <p className='menu'>Ver Menu</p></Link>
             </div>
         </div>
           <img src="https://images.unsplash.com/photo-1579751626657-72bc17010498?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="" />
